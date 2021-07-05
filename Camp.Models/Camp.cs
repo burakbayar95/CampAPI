86b7c2610fec8923a579e6539dae11967fe0bd8c
Burak <95burakbayar@gmail.com>
2021-07-05 23:32:46 +0300

    T

---
+++ Camp.Models/Camp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camp.Models
+{
+    public class Camp : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        //Navigation Property
+        public virtual Genre Genre { get; set; }
+
+
+    }
+}
---
+++ Camp.Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Camp.Models
+{
+    public class Genre : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı boş geçilemez")]
+        public int Name { get; set; }
+
+        public virtual IList<Camp> Camps { get; set; }
+
+
+    }
+}
